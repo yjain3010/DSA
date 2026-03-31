1d2b1c1b1a6cdd3cc61430f53cfb1e196ed5214e
yjain3010 <yjain301000@gmail.com>
2026-04-01 01:34:49 +0530

    Three Sum

---
+++ Two Pointers/Three Sum.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++) {
+            // Optimization: If the smallest number is > 0, sum can't be 0
+            if (nums[i] > 0) break;
+
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    
+                    // Move pointers and skip duplicates
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } 
+                else if (sum < 0) {
+                    j++; // Need a larger sum
+                } 
+                else {
+                    k--; // Need a smaller sum
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    
+    vector<vector<int>> result = sol.threeSum(nums);
+
+    cout << "Triplets that sum to zero:" << endl;
+    for (const auto& triplet : result) {
+        cout << "[" << triplet[0] << ", " << triplet[1] << ", " << triplet[2] << "]" << endl;
+    }
+
+    return 0;
+}
